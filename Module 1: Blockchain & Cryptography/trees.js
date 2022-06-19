2ba068b255877e9726392e6a4a8ee1ec3ed8fdb4
Kyle Zheng <kylezheng73@gmail.com>
2022-06-19 12:39:16 -0400

    debug

--- Module 1: Blockchain & Cryptography/trees.js
+++ Module 1: Blockchain & Cryptography/trees.js
@@ -29,10 +29,12 @@ const dataStorage = new Map();
 let rootHash;
 
 const buildTree = () => {
+  console.debug("BUILD TREE");
   const leafNodeHashes = Array.from(dataStorage.keys()).sort();
+  console.debug("leafNodeHashes", leafNodeHashes);
 
-  const remainingNodes = leafNodeHashes.map((hash) => ({ value: hash }));
-  const parentNodes = [];
+  let remainingNodes = leafNodeHashes.map((hash) => ({ value: hash }));
+  let parentNodes = [];
   for (let i = 0; i < remainingNodes.length; i += NUM_CHILDREN) {
     const siblingNodes = remainingNodes.slice(i, i + NUM_CHILDREN);
     const siblingHashes = siblingNodes.map((node) => node.value);
@@ -41,27 +43,70 @@ const buildTree = () => {
       value: testHash(...siblingHashes),
       children: siblingHashes,
     };
+
+    console.debug({ parentNode });
     parentNodes.push(parentNode);
     treeStorage.set(parentNode.value, parentNode);
 
     // go to next level
+    console.debug({ i, remainingNodes, parentNodes });
     if (i + NUM_CHILDREN >= remainingNodes.length && parentNodes.length > 1) {
       remainingNodes = parentNodes;
       parentNodes = [];
       i = 0;
+      console.debug("NEXT LEVEL");
     }
   }
 
   rootHash = parentNodes[0].value;
+  console.debug({ rootHash });
 };
 
-const displayTree = () => {};
+const drawTree = () => {
+  const queue = [rootHash, null];
+
+  let depth = 0;
+  let siblingIndex = 0;
+
+  while (queue.length) {
+    let currHash = queue.shift();
+    if (currHash == null) {
+      if (!queue.length) break;
+
+      queue.push(null);
+      depth++;
+      siblingIndex = 0;
+      continue;
+    }
+
+    let currNode = treeStorage.get(currHash);
+
+    drawNode(currNode.value, depth, siblingIndex);
+
+    siblingIndex++;
+  }
+};
+
+const drawNode = (text, depth, siblingIndex) => {
+  const node = document.createElement("div");
+  const levelHeight = 50;
+  const nodeWidth = 100;
+  node.textContent = text;
+  node.style.transform = `translate(${depth * levelHeight}px, ${
+    siblingIndex * nodeWidth
+  })`;
+
+  treeBox.appendChild(node);
+};
 
 const insertLeaf = (value) => {
   const key = testHash(value);
   dataStorage.set(key, value);
 
+  treeStorage.clear();
+  treeBox.textContent = "";
   buildTree();
+  drawTree();
   //storage.set(key, { data: value });
 
   // const rootKey = storage.keys[0];
@@ -72,3 +117,5 @@ const controlBox = document.getElementById("controlBox");
 const storageBox = document.getElementById("storageBox");
 const addButton = document.getElementById("addButton");
 const dataInput = document.getElementById("dataInput");
+
+addButton.addEventListener("click", () => insertLeaf(dataInput.value));
