375ad5949574955bb3c2932d47f7950b346dd940
Kyle Zheng <kylezheng73@gmail.com>
2022-06-21 00:51:43 -0400

    tree vis controls

--- Module 1: Blockchain & Cryptography/trees.js
+++ Module 1: Blockchain & Cryptography/trees.js
@@ -29,9 +29,7 @@ const dataStorage = new Map();
 let rootHash;
 
 const buildTree = () => {
-  console.debug("BUILD TREE");
   const leafNodeHashes = Array.from(dataStorage.keys()).sort();
-  console.debug("leafNodeHashes", leafNodeHashes);
 
   let remainingNodes = leafNodeHashes.map((hash) => ({ value: hash }));
   let parentNodes = [];
@@ -44,32 +42,33 @@ const buildTree = () => {
       children: siblingHashes,
     };
 
-    console.debug({ parentNode });
     parentNodes.push(parentNode);
     treeStorage.set(parentNode.value, parentNode);
 
-    // go to next level
-    console.debug({ i, remainingNodes, parentNodes });
     if (i + NUM_CHILDREN >= remainingNodes.length && parentNodes.length > 1) {
       remainingNodes = parentNodes;
       parentNodes = [];
-      i = 0;
-      console.debug("NEXT LEVEL");
+      i = -NUM_CHILDREN;
     }
   }
 
   rootHash = parentNodes[0].value;
-  console.debug({ rootHash });
+  console.debug(Array.from(treeStorage.entries()).map(([k, v]) => ({ k, v })));
 };
 
-const drawTree = () => {
+const drawTree = (container) => {
+  container.textContent = "";
   const queue = [rootHash, null];
 
   let depth = 0;
   let siblingIndex = 0;
 
+  const treeHeight = Math.ceil(
+    Math.log(dataStorage.size) / Math.log(NUM_CHILDREN)
+  );
+
   while (queue.length) {
-    let currHash = queue.shift();
+    const currHash = queue.shift();
     if (currHash == null) {
       if (!queue.length) break;
 
@@ -79,24 +78,47 @@ const drawTree = () => {
       continue;
     }
 
-    let currNode = treeStorage.get(currHash);
+    const currNode = treeStorage.get(currHash);
+    const text = currHash;
+    // text = currNode.value;
+    if (currNode) {
+      queue.push(...currNode.children);
+    }
 
-    drawNode(currNode.value, depth, siblingIndex);
+    drawNode(container, text, treeHeight, depth, siblingIndex);
 
     siblingIndex++;
   }
 };
 
-const drawNode = (text, depth, siblingIndex) => {
-  const node = document.createElement("div");
-  const levelHeight = 50;
-  const nodeWidth = 100;
-  node.textContent = text;
-  node.style.transform = `translate(${depth * levelHeight}px, ${
-    siblingIndex * nodeWidth
-  })`;
+/**
+ *  Braindead node positioning
+ *  Don't look at this, use a library pls
+ */
+const drawNode = (container, text, treeHeight, depth, siblingIndex) => {
+  const node = document.createElement("span");
 
-  treeBox.appendChild(node);
+  node.textContent = text;
+  node.style.position = "absolute";
+
+  const invHeight = treeHeight - depth;
+
+  const leftOffsetUnits = invHeight ? Math.pow(2, invHeight - 1) - 1 / 2 : 0;
+  const leftOffset = leftOffsetUnits * spacing;
+
+  const siblingOffset = siblingIndex * Math.pow(2, invHeight) * spacing;
+
+  const leftDistance = leftOffset + siblingOffset;
+
+  node.style.transform = `translate(${leftDistance}px, ${
+    depth * nodeHeight
+  }px)`;
+  node.style.width = `${nodeWidth}px`;
+  node.style.height = `${nodeHeight}px`;
+
+  node.style.backgroundColor = "red";
+
+  container.appendChild(node);
 };
 
 const insertLeaf = (value) => {
@@ -104,18 +126,75 @@ const insertLeaf = (value) => {
   dataStorage.set(key, value);
 
   treeStorage.clear();
-  treeBox.textContent = "";
   buildTree();
-  drawTree();
+
+  drawTree(treeBox);
   //storage.set(key, { data: value });
 
   // const rootKey = storage.keys[0];
 };
 
-const treeBox = document.getElementById("treeBox");
-const controlBox = document.getElementById("controlBox");
-const storageBox = document.getElementById("storageBox");
-const addButton = document.getElementById("addButton");
-const dataInput = document.getElementById("dataInput");
+const updateLeaf = (key) => {
+  dataStorage.set(key, value);
+};
 
-addButton.addEventListener("click", () => insertLeaf(dataInput.value));
+/**
+ * Initializing Stuffs
+ */
+const el = Object.fromEntries(
+  [
+    "treeBox",
+    "dataInput",
+    "dataForm",
+    "nodeWidthInput",
+    "nodeWidthText",
+    "nodeHeightInput",
+    "nodeHeightText",
+    "spacingInput",
+    "spacingText",
+  ].map((id) => {
+    const element = document.getElementById(id);
+    return [id, element];
+  })
+);
+
+let nodeHeight;
+let nodeWidth;
+let spacing;
+
+const updateNodeWidth = (width) => {
+  nodeWidth = width;
+  el.nodeWidthText.textContent = `${nodeWidth} px`;
+};
+const updateNodeHeight = (height) => {
+  nodeHeight = height;
+  el.nodeHeightText.textContent = `${nodeHeight} px`;
+};
+const updateSpacing = (newSpacing) => {
+  spacing = newSpacing;
+  el.spacingText.textContent = `${spacing} px`;
+};
+
+updateNodeWidth(60);
+updateNodeHeight(40);
+updateSpacing(80);
+
+el.nodeWidthInput.addEventListener("input", (e) =>
+  updateNodeWidth(e.target.value)
+);
+el.nodeHeightInput.addEventListener("input", (e) =>
+  updateNodeHeight(e.target.value)
+);
+el.spacingInput.addEventListener("input", (e) => updateSpacing(e.target.value));
+
+el.nodeWidthInput.addEventListener("change", () => drawTree(treeBox));
+el.nodeHeightInput.addEventListener("change", () => drawTree(treeBox));
+el.spacingInput.addEventListener("change", () => drawTree(treeBox));
+
+el.dataForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  insertLeaf(dataInput.value);
+  dataInput.value = "";
+});
+
+// addButton.addEventListener("click", () => insertLeaf(dataInput.value));
